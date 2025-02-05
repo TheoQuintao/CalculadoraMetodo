c4faae3cb372cd8891e25631c22adc2c682b6b7b
Theo Quintão <0000900198@senaimgaluno.com.br>
2025-02-05 10:29:50 -0300

    Add Operações

---
+++ calculadora/Operacoes.cs
@@ -0,0 +1,85 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Operacoes
+{
+    public class Calculos
+    {
+        public static void soma(
+            Decimal Valor1,
+            Decimal Valor2
+        )
+        {
+            Facilitador.Loading(2);
+            Console.WriteLine($"O resultado da soma de {Valor1} e {Valor2} é {Valor1+Valor2}");
+        }
+        public static void Subtração(
+            Decimal Valor1,
+            Decimal Valor2
+        )
+        {
+            Facilitador.Loading(2);
+            Console.WriteLine($"O resultado da subtração de {Valor1} e {Valor2} é {Valor1-Valor2}");
+        }
+        public static void Multiplicação(
+            Decimal Valor1,
+            Decimal Valor2
+        )
+        {
+            Facilitador.Loading(2);
+            Console.WriteLine($"O resultado da multiplicação de {Valor1} e {Valor2} é {Valor1*Valor2}");
+        }
+        public static void Divizão(
+            Decimal Valor1,
+            Decimal Valor2
+        )
+        {
+            Facilitador.Loading(2);
+            Console.WriteLine($"O resultado da divizão de {Valor1} e {Valor2} é {Valor1/Valor2}");
+        }
+    }
+    public class Facilitador
+    {
+        public static decimal ValorDecimal(int ordinal)
+        {
+            return1:
+            Console.Clear();
+            Console.Write($"Escreva o {ordinal}º valor numerico: ");
+            if(!decimal.TryParse(Console.ReadLine(), out decimal valor))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Valor invalido!!");
+                Thread.Sleep(1500);
+                Console.ResetColor();
+                goto return1;
+            }
+
+            return valor;
+        }
+        public static void Pausa()
+        {
+            Console.WriteLine();
+            Console.Write("Aperte ENTER para continuar");
+            Console.ReadLine();
+
+        }
+        public static void Loading(
+            int Tempo
+        )
+        {
+
+            for(int i = 0; i<=Tempo; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Loading.");
+                Thread.Sleep(333);
+                Console.Clear();
+                Console.WriteLine("Loading..");
+                Thread.Sleep(333);
+                Console.Clear();
+                Console.WriteLine("Loading...");
+                Thread.Sleep(333);
+            }
+        }
+    }
+
+}
---
+++ calculadora/Program.cs
@@ -0,0 +1,57 @@
+﻿using Operacoes;
+
+while(true)
+{
+    return1:
+    Console.Clear();
+    Console.WriteLine(
+        "Qual Operação deseja realizar? \n\n(1) Somar \n(2) Subtração \n(3) Multiplicação \n(4) Divisão \n(5) Sair"
+    );
+    if(!int.TryParse(Console.ReadLine(), out int Menu))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Valor invalido!!");
+        Thread.Sleep(1500);
+        Console.ResetColor();
+        goto return1;
+    }
+    
+    switch(Menu)
+    {
+        case 1:
+            Calculos.soma(
+                Facilitador.ValorDecimal(1),
+                Facilitador.ValorDecimal(2)
+            );
+            Facilitador.Pausa();
+        break;
+
+        case 2:
+            Calculos.Subtração(
+                Facilitador.ValorDecimal(1),
+                Facilitador.ValorDecimal(2)
+            );
+            Facilitador.Pausa();
+        break;
+
+        case 3:
+            Calculos.Multiplicação(
+                Facilitador.ValorDecimal(1),
+                Facilitador.ValorDecimal(2)
+            );
+            Facilitador.Pausa();
+        break;
+
+        case 4:
+            Calculos.Divizão(
+                Facilitador.ValorDecimal(1),
+                Facilitador.ValorDecimal(2)
+            );
+            Facilitador.Pausa();
+        break;
+
+        case 5:
+        return;
+
+    }
+}
